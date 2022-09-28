a1ba5d5be7b7720a56cfb2f68256b240cec46c24
Marko Maturino <76288157+hybridmarko720@users.noreply.github.com>
2022-09-27 19:15:14 -0500

    Backup

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,35 @@
 import { Link } from "@mui/material"
 import logo from "../imgs/Logo.png"
+import {Button} from "@mui/material"
+import {TextField} from "@mui/material"
+import {FormControl} from "@mui/material"
+const url = "https://api.mwi.dev/content/contact"
+let x = ""
+let paragraphResult = ""
+// data.data[0].content
 
-
+// const getData = () => fetch(url).then(response => response.json()).then((results) =>{
+//   let result = results.data[0].content
+//   resolve(result)
+// } );  
+async function getData(){
+  return new Promise((resolve, reject)=>{
+    fetch(url)
+    .then(response => {return response.json})
+    .then(data => {
+      let result = data.data[0].content
+       resolve(result)
+    })
+  })
+  
+}
+await getData().then(data => {
+  movies = data
+});
 function Contact() {
+
   return (
+  
     <div>
     
     <div className="Contact" style={{display: "flex", margin: "0"}}>
@@ -12,29 +38,43 @@ function Contact() {
       body {
         margin: 0px;
         padding: 0px;
+       
       }`}</style>
-        <div className="Left" style={{display: "flex", height: "100vh", width: "50vw", flexDirection: "column"} }> 
-        <img src = {logo} alt = "Midwestern Interative" className="Logo" style={{alignSelf: "start", justifySelf: "start", marginLeft: "5vw", marginTop: "5vh"}}></img>
-        <div style = {{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginTop: "20vh"}}>
-        <h1 style={{color: "white", fontSize: "60px"}}><span style = {{textDecoration: "underline", textDecorationColor: "#DEBF79", textUnderlineOffset: "20px", textDecorationThickness: "4px"}}>Heading</span> One</h1>
-            <p style={{color: "white", fontSize: "17.5px", width:"32vw"}}>Lorem ipsum dolor sit amet, consecteur adipiscing elit, sed do dos eiusmod tempor incididunt ut labore et trace dolore magna alique</p>
-            <p style={{color: "white", fontSize: "17.5px", width:"32vw"}}>Proin sagittis nisl rhoncus mattis rhoncus. At augue eget arcu dictum varius duis at consecteur Lorem</p>
+        <div className="Left" style={{display: "flex", height: "100vh", width: "50vw", flexDirection: "column", outline: "2px solid red", justifyContent: "space-between" } }> 
+          <img src = {logo} alt = "Midwestern Interative" className="Logo" style={{alignSelf: "start", marginLeft: "5vw", marginTop: "5vh"}}></img>
+            
+          <div style = {{display: "flex",flexDirection: "column", justifyContent: "center", color: "white", marginLeft: "5vw"}}>
+              <h1 style={{fontSize: "60px"}}><span style = {{textDecoration: "underline", textDecorationColor: "#DEBF79", textUnderlineOffset: "20px", textDecorationThickness: "4px"}}>Heading</span> One</h1>
+              <p style={{fontSize: "17.5px", width:"32vw",margin: "0"}}>{}</p>
+  
           </div>
+        <p></p>
         </div>
-      <div className="Right" style={{display: "flex", height: "100vh", width: "50vw", backgroundColor:"white"}}>
-      <Link style= {{fontSize: 20, color: "#DEBF79"}} underline="none">home</Link>
-
-        <form action="/my-handling-form-page" method="post">
-  
-    
-      <input type="text" id="name" name="user_name" placeholder="name"/>
-  
-      <input type="email" id="mail" name="user_email" placeholder="Email"/>
- 
       
-      <textarea id="msg" name="user_message"placeholder="Message"></textarea>
- 
-      </form>
+      <div className="Right" style={{display: "flex", height: "100vh", width: "50vw", backgroundColor:"white", flexDirection: "column", justifyContent: "space-between"}}>
+        <Link style= {{fontSize: 20, color: "#DEBF79", alignSelf: "end", marginTop:"5vh", marginRight: "5vw"}} underline="none">home</Link>
+        <div className="MainForm" style={{display: "flex", justifyContent:"center"}}>
+          <form action="" method="post">
+        
+            <h2 style = {{fontSize: "30px"}}>Heading two</h2>
+            <div className="firstAndLast" style={{display: "flex", gap: "10px", marginBottom: "1vw"}}>
+              <TextField id="firstName" label="First Name" style = {{backgroundColor: "#F5F5F5"}} />
+              <TextField id="lastName" label="Last Name" style = {{backgroundColor: "#F5F5F5"}} />
+            </div>
+            <div style={{display: "flex", gap: "10px", marginBottom: "1vw"}}>
+              <TextField id="title" label="Title" style = {{backgroundColor: "#F5F5F5"}}/>
+              <TextField id="email" label="Email" style = {{backgroundColor: "#F5F5F5"}} />
+            </div>
+            <div style={{display: "flex", marginBottom: "1vw"}}>
+              <TextField id="message" label="Message" multiline minRows={4}  fullWidth style = {{backgroundColor: "#F5F5F5"}}/>
+            </div>
+            <div style = {{display: "flex", justifyContent: "center"}}>
+            <Button size="large" variant="containted" color = "neutral" style = {{ textTransform: "none", backgroundColor:"#DEBF79", color: "white", borderRadius: 0, marginTop: "1vh"}} >Submit</Button>
+            </div>
+          </form>
+        </div>
+      
+      <p></p>
 
 
         </div>
--- src/Components/Headings.js
+++ src/Components/Headings.js
@@ -21,7 +21,7 @@ function Headings(props) {
     <img src = {props.image}></img>
       
         <h1 className = "h1InsideHeadings" style={{fontSize: "2rem", marginBottom: 0}}>Heading Two</h1>
-        <p className = "pInsideHeadings" style={{color: "#858585", fontSize: ".1em", width:"75%", lineHeight: "20px", textAlign: "left", wordSpacing: ".5em", fontSize: "15px"}}>Intenger accumsan molestie nisl, id faucibus urna accumsan quis. Proin vulputate, mauris semper maximus</p>
+        <p className = "pInsideHeadings" style={{color: "#858585", fontSize: ".1em", width:"75%", lineHeight: "20px", textAlign: "left", wordSpacing: ".5em", fontSize: "15px", borderRadius: 0}}>Intenger accumsan molestie nisl, id faucibus urna accumsan quis. Proin vulputate, mauris semper maximus</p>
       
        
     <Button size="large" variant="containted" color = "neutral" style = {{ textTransform: "none", backgroundColor:"#DEBF79", color: "white", borderRadius: 0}}>Learn More</Button>
